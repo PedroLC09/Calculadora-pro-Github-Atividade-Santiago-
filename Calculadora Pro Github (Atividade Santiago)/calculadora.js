6e18ae117d0a96a699a8f0f0a538656b7c705825
Peuzz o Dev <pedro_lc_correia@estudante.sesisenai.org.br>
2025-11-17 15:35:46 -0300

    Add files via upload

---
+++ Calculadora Pro Github (Atividade Santiago)/calculadora.js
@@ -0,0 +1,5 @@
+//Cria uma variável e guarda nela o elemento que tem o id indicado
+let elementoInputUm = document.getElementById("inpt01")
+let elementoInputDois = document.getElementById("inpt02")
+
+
